6924c099d98dbe4a12f3766168da101d55b4e656
bachhungcb <bachhungcb@gmail.com>
2024-09-22 10:14:28 +0700

    .

--- src/typeof.java
+++ src/typeof.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+
 
 public class typeof {
+    public static void main(String[] args) {
+        char c = 't';
+        int j;
+
+        System.out.println("Hello world!");
+    }
 }
